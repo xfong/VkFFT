da0fd28056e24874ea7289ab193ef85ca8658d51
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2021-09-01 21:09:40 +0800

    begin interface test

---
+++ ownTest/make_cpp.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IFN=${1}
+
+FLAGS="-O4 -fPIC"
+CPATHS="-I${CUDA_PATH}/include"
+LDPATHS="-L${CUDA_PATH}/lib64"
+LIBS="-lOpenCL -lm"
+
+g++ ${FLAGS} ${CPATHS} ${LDPATHS}/lib64 ${IFN}.cpp ${LIBS} -o ${IFN}
