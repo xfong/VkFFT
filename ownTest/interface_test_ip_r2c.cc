40fe9e22f37fe94a6c87a71ece9f85212d205a74
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2021-09-01 22:48:20 +0800

    Updated test code

--- ownTest/interface_test_ip_r2c.cc
+++ ownTest/interface_test_ip_r2c.cc
@@ -84,8 +84,84 @@ int main() {
         printf("Unable to bake plan...abort\n");
         return -1;
     }
+
+    // Create date for testing...
+    printf("    Begin testing...\n");
+
+    // Allocate memory
+    printf("Allocating memory...\n");
+    size_t inputElements    = lengths[0]*lengths[1]*lengths[2];
+    size_t outputElements   = (lengths[0] /2 + 1)*lengths[1]*lengths[2];
+    size_t inputBufferSize  = sizeof(float)*inputElements;
+    size_t outputBufferSize = 2*sizeof(float)*outputElements;
+
+    float* input1  = (float*) malloc(inputBufferSize);
+    float* output1 = (float*) malloc(outputBufferSize);
+
+    // Allocate GPU buffers
+    printf("Creating buffers...\n");
+    cl_mem iBuf = clCreateBuffer(context, CL_MEM_READ_WRITE, inputBufferSize, NULL, &res);
+    if (res != CL_SUCCESS) {
+        printf("Failed to allocate buffer for input...aborting\n");
+        return -1;
+    }
+    cl_mem oBuf = clCreateBuffer(context, CL_MEM_READ_WRITE, outputBufferSize, NULL, &res);
+    if (res != CL_SUCCESS) {
+        printf("Failed to allocate buffer for output...aborting\n");
+        return -1;
+    }
+
+    // First data set
+    printf("Generating first data set on host side...\n");
+    input1[0] = (float)(rand()) / (float)(RAND_MAX);
+    for (uint64_t idx = 0; idx < inputElements; idx++) {
+        input1[idx] = (float)(0.0);
+    }
+
+    printf("Generating first data set (output) on host side...\n");
+    for (uint64_t idx = 0; idx < outputElements; idx++) {
+        output1[idx] = (float)(0.0);
+    }
+
+    // Copy CPU data over to GPU
+    printf("Transferring first data set to GPU side...\n");
+    res = clEnqueueWriteBuffer(commandQueue, iBuf, true, 0, inputBufferSize, input1, 0, NULL, NULL);
+    if (res != CL_SUCCESS) {
+        printf("Failed to write into buffer for input...aborting\n");
+        return -1;
+    }
+    res = clEnqueueWriteBuffer(commandQueue, oBuf, true, 0, outputBufferSize, output1, 0, NULL, NULL);
+    if (res != CL_SUCCESS) {
+        printf("Failed to write into buffer for output...aborting\n");
+        return -1;
+    }
+
+    // Second data set
+    printf("Generating second data set on host side...\n");
+    for (uint64_t idx = 0; idx < inputElements; idx++) {
+        input1[idx] = (float) rand() / (float)(RAND_MAX);
+    }
+
+    printf("Generating second data set (output) on host side...\n");
+    for (uint64_t idx = 0; idx < outputElements; idx++) {
+        output1[idx] = (float) rand() / (float)(RAND_MAX);
+    }
+
+    // Exiting...
     printf("Exiting...\n");
     DestroyFFTPlan(plan);
+    res = clReleaseMemObject(iBuf);
+    if (res != CL_SUCCESS) {
+        printf("Failed to release input buffer...aborting\n");
+        return -1;
+    }
+    res = clReleaseMemObject(oBuf);
+    if (res != CL_SUCCESS) {
+        printf("Failed to release output buffer...aborting\n");
+        return -1;
+    }
+    free(input1);
+    free(output1);
 
     return 0;
 }
