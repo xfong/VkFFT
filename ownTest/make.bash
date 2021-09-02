17b597d708152f4b293ad68a91211a4270ca6a9c
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2021-09-02 10:34:15 +0800

    fix make.bash

--- ownTest/make.bash
+++ ownTest/make.bash
@@ -5,10 +5,10 @@ IFN=${1}
 FLAGS="-O4 -fPIC"
 CPATHS="-I${CUDA_PATH}/include"
 LDPATHS="-L${CUDA_PATH}/lib64 -L./"
-LIBS="-lOpenCL -lvkfft -lm"
+LIBS="-lOpenCL -lm"
 
 if [[ "fft_interface" == ${IFN} ]]; then
     gcc ${FLAGS} ${CPATHS} ${LDPATHS} fft_interface.cc ${LIBS} -shared -o libvkfft.so
 else
-    gcc ${FLAGS} ${CPATHS} ${LDPATHS} ${IFN}.cc ${LIBS} -o ${IFN}
+    gcc ${FLAGS} ${CPATHS} ${LDPATHS} ${IFN}.cc ${LIBS} -lvkfft -o ${IFN}
 fi
