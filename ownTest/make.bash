9be9f909ceec6558356e8fae15b252faf1173700
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2021-09-02 10:29:08 +0800

    Update make scripts

--- ownTest/make.bash
+++ ownTest/make.bash
@@ -4,7 +4,11 @@ IFN=${1}
 
 FLAGS="-O4 -fPIC"
 CPATHS="-I${CUDA_PATH}/include"
-LDPATHS="-L${CUDA_PATH}/lib64"
-LIBS="-lOpenCL -lm"
+LDPATHS="-L${CUDA_PATH}/lib64 -L./"
+LIBS="-lOpenCL -lvkfft -lm"
 
-gcc ${FLAGS} ${CPATHS} ${LDPATHS} ${IFN}.cc ${LIBS} -o ${IFN}
+if [[ "fft_interface" == ${IFN} ]]; then
+    gcc ${FLAGS} ${CPATHS} ${LDPATHS} fft_interface.cc ${LIBS} -shared -o libvkfft.so
+else
+    gcc ${FLAGS} ${CPATHS} ${LDPATHS} ${IFN}.cc ${LIBS} -o ${IFN}
+fi
