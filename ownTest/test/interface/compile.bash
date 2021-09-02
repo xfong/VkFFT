258d43015f3e36857db47d004578e52cf6b9e6d3
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2021-09-02 22:18:46 +0800

    refactored vkfft interface

--- ownTest/test/interface/compile.bash
+++ ownTest/test/interface/compile.bash
@@ -3,9 +3,9 @@
 IFN=${1}
 
 FLAGS="-O4 -fPIC -Wall"
-CPATHS="-I${CUDA_PATH}/include -std=c99"
-LDPATHS="-L${CUDA_PATH}/lib64"
-LIBS="-lOpenCL -lm"
+CPATHS="-I${CUDA_PATH}/include -I../../include -std=c99"
+LDPATHS="-L${CUDA_PATH}/lib64 -L../../lib"
+LIBS="-lOpenCL -lvkfft -lm"
 
 if [ -f ${IFN}.o ]; then
     rm ${IFN}.o
