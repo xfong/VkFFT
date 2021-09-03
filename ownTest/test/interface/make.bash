1dd1e1e37a5a62ae48815dde70adaa9a4bd29df1
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2021-09-03 09:48:41 +0800

    Update test

--- ownTest/test/interface/make.bash
+++ ownTest/test/interface/make.bash
@@ -2,7 +2,7 @@
 
 IFN=${1}
 
-FLAGS="-O4 -fPIC -Wall"
+FLAGS="-O4 -fPIC -Wall -D__DEBUG__=0"
 CPATHS="-I${CUDA_PATH}/include -I../../include"
 LDPATHS="-L${CUDA_PATH}/lib64 -L../../lib"
 LIBS="-lOpenCL -lvkfft -lm"
